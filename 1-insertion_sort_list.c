bc0a1d516f216f1e27655e8fbd2a180cbfe61156
omarbdw <omarbdw@yahoo.com>
2023-12-14 17:37:06 +0200

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *temp;
+    if (!list || !(*list) || !(*list)->next)
+        return;
+    current = (*list)->next;
+    while (current)
+    {
+        temp = current;
+        while (temp->prev && temp->n < temp->prev->n)
+        {
+            temp->prev->next = temp->next;
+            if (temp->next)
+                temp->next->prev = temp->prev;
+            temp->next = temp->prev;
+            temp->prev = temp->prev->prev;
+            temp->next->prev = temp;
+            if (!temp->prev)
+                *list = temp;
+            else
+                temp->prev->next = temp;
+            print_list(*list);
+        }
+        current = current->next;
+    }
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,7 @@
+#include "sort.h"
+void selection_sort(int *array, size_t size)
+{
+    int i, j;
+    listint_t temp;
+    
+}
